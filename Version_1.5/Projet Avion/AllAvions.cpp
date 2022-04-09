5753e36824edc160678d347e4f95e1f64927437a
gendrecharlotte <99488785+gendrecharlotte@users.noreply.github.com>
2022-04-09 19:43:56 +0200

    Add files via upload

---
+++ Version_1.5/Projet Avion/AllAvions.cpp
@@ -0,0 +1,54 @@
+#include "AllAvions.h"
+
+///CONSTRUCTEURS/DESTRUCTEURS
+AllAvions::AllAvions(){}
+AllAvions::~AllAvions(){}
+AllAvions::AllAvions(std::string nomFichier) //le constructeur lit le fichier pour consruire le vecteur de tous les avions
+{
+    std::ifstream ifs{nomFichier};
+    if (!ifs)
+        throw std::runtime_error( "Impossible d'ouvrir en lecture " + nomFichier ); //exception
+    int nb_avions; //lecture du nombre d'avions
+    ifs >> nb_avions;
+    if ( ifs.fail() )
+        throw std::runtime_error("Probleme lecture nombre avions"); //exception
+    int matricule,type,consomation,capacite, vitesse; //temps pour création d'un nouvel avion
+    BITMAP *img;
+    Coordonnees coo(0,0);
+    //std::stack<Aeroport*> trajet;
+    for(int i=0;i<nb_avions;i++)
+    {
+        ifs>>matricule>>type>>consomation>>capacite>>vitesse; //lecture données avion
+        if ( ifs.fail() )
+            throw std::runtime_error("Probleme lecture avion"); //exception
+        /*if(type==3)
+            img=load_bitmap("images/avion_3.bmp",NULL);
+        else if(type==2)
+            img=load_bitmap("images/avion_2.bmp",NULL);
+        else if(type==1)
+            img=load_bitmap("images/avion_1.bmp",NULL);
+        if (!img)
+        {
+            allegro_message("pas pu trouver/charger image");
+            allegro_exit(); exit(EXIT_FAILURE);
+        }*/
+
+        m_liste_avions.push_back(new Avion(matricule,type,consomation,capacite,vitesse, img,coo,false/*,trajet*/)); //création+ajout au vecteur
+    }
+}
+///GETTERS/SETTERS
+std::vector<Avion*> AllAvions::get_liste_avions(){return m_liste_avions;}
+void AllAvions::set_liste_avions(std::vector<Avion*> val){m_liste_avions=val;}
+///AFFICHAGE
+void AllAvions::affichage()
+{
+    std::vector<Avion*> vect=get_liste_avions();
+    for(auto a:vect)
+    {
+        std::cout<<"Matricule : "<<a->get_matricule();
+        std::cout<<"    Type : "<< a->get_type();
+        std::cout<<"    Consommation : "<<a->get_consomation()<<" L/km";
+        std::cout<<"    Capacite reservoir : "<<a->get_capacite()<<" L";
+        std::cout<<"     Vitesse  : "<<a->get_vitesse()<<"km/h"<<std::endl;
+    }
+}
---
+++ Version_1.5/Projet Avion/Avion.h
@@ -0,0 +1,52 @@
+#ifndef AVION_H_INCLUDED
+#define AVION_H_INCLUDED
+
+#include "Coordonnees.h"
+//#include "Aeroport.h"///////////////////////////////////////////////////////////////////aled
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+class Avion
+{
+private:
+    ///ATTRIBUTS
+    int m_matricule;
+    int m_type;
+    int m_consomation;
+    int m_capacite;
+    int m_vitesse;
+    BITMAP *m_img;
+    Coordonnees m_coo;
+    bool m_da;
+    //std::stack<Aeroport*> m_trajet;
+public:
+    ///CONSTRUCTEURS/DESTRUCTEURS
+    Avion();
+    Avion(int vitesse,int matricule,int type, int consomation, int capacite,BITMAP *img,Coordonnees coo, bool da/*,std::stack<Aeroport*> trajet*/);
+    ~Avion();
+    ///GETTERS/SETTERS
+    int get_vitesse()const;
+    int get_matricule()const;
+    int get_type()const;
+    int get_consomation()const;
+    int get_capacite()const;
+    BITMAP *get_img()const;
+    Coordonnees get_coo()const;
+    bool get_da()const;
+    //std::stack<Aeroport*> get_trajet()const;
+    void set_vitesse(int val);
+    void set_matricule(int val);
+    void set_type(int val);
+    void set_consomation(int val);
+    void set_capacite(int val);
+    void set_img(BITMAP *val);
+    void set_coo(Coordonnees val);
+    void set_da(bool da);
+    //void set_trajet(std::stack<Aeroport*> val);
+};
+
+#endif // AVION_H_INCLUDED
