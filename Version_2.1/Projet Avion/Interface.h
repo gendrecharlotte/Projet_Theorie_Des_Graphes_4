0fca6b5e233eb2cea8134eace988b2c107a6b629
gendrecharlotte <99488785+gendrecharlotte@users.noreply.github.com>
2022-04-10 20:39:00 +0200

    Add files via upload

---
+++ Version_2.1/Projet Avion/Interface.h
@@ -0,0 +1,22 @@
+#ifndef INTERFACE_H_INCLUDED
+#define INTERFACE_H_INCLUDED
+
+#include "Simulation.h"
+
+class Interface
+{
+private:
+    ///ATTRIBUTS
+
+public:
+    ///CONSTRUCTEURS/DESTRUCTEURS
+    Interface();
+    ~Interface();
+    ///METHODES
+    void affichage_interface(DifferentsAeroports aeroports,int type);
+    void interface_simulation(AllAvions avions, DifferentsAeroports aeroports, Simulation simulation);
+    void test(AllAvions avions, DifferentsAeroports aeroports, Simulation simulation);
+    void tracer_chemin(Voyageur& voyageur);
+};
+
+#endif // INTERFACE_H_INCLUDED
