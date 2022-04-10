1fea10bb2a52c9a6ff456e4db595695afd427aba
gendrecharlotte <99488785+gendrecharlotte@users.noreply.github.com>
2022-04-10 10:45:55 +0200

    Add files via upload

---
+++ Version_2.0/Projet Avion/Aeroport.h
@@ -0,0 +1,76 @@
+#ifndef HEADERAERO_H_INCLUDED
+#define HEADERAERO_H_INCLUDED
+
+#include "AllAvions.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class InfosAeroport
+{
+
+    private:
+    int m_Num;
+    std::string m_Nom;
+    Coordonnees m_Localisation;
+    int m_Nombres_Pistes;
+    int m_Nombres_Places_Sol;
+    int m_Attente_Sol;
+    int m_Temps_Acces_Piste;
+    int m_Delai_Anticolision;
+    int m_Temps_Decollage_Atterissage;
+    int m_Duree_Attente_Vol;
+    int m_Distance;
+    std::vector<const InfosAeroport*> m_successeurs;
+    std::vector< int > m_poids;
+    std::vector<Avion*> m_avions_sur_pistes;
+    std::vector<Avion*> m_avions_au_sol;
+
+    public:
+
+    InfosAeroport();
+    InfosAeroport(int Num,std::string Nom, Coordonnees Localisation, int Nombre_Pistes, int Nombres_Places_Sol,  int Attente_Sol,
+     int Temps_Acces_Piste,  int Delai_Anticolision, int Temps_Decollage_Atterissage, int Duree_Attente_Vol,
+     int Distance,std::vector<Avion*> avions_sur_pistes,std::vector<Avion*> avions_au_sol);
+    ~InfosAeroport();
+
+    void init_poids(int taille_s);
+
+    int getNum() const;
+    std::string get_Nom() ;
+    Coordonnees get_Localisation() ;
+    int get_Nombres_Pistes() ;
+    int get_Nombres_Places_Sol() ;
+    int get_Attente_Sol() ;
+    int get_Temps_Acces_Piste() ;
+    int get_Delai_Anticolision() ;
+    int get_Temps_Decollage_Atterissage() ;
+    int get_Duree_Attente_Vol() ;
+    int get_Distance() ;
+    std::vector<Avion*> get_avions_sur_pistes();
+    std::vector<Avion*> get_avions_au_sol();
+
+    const std::vector<const InfosAeroport*>& getSuccesseurs()const;
+    std::vector<int> getpoids()const;
+
+    void set_Nom(std::string n);
+    void set_Localisation(Coordonnees L);
+    void set_Pistes(int P);
+    void set_Places(int Pla);
+    void set_Attente(int A);
+    void set_Acces(int Ac);
+    void set_Delai(int D);
+    void set_Decollage(int Dec);
+    void set_Duree(int Du);
+    void set_Distance(int Dis);
+    void set_avions_sur_pistes(std::vector<Avion*> val);
+    void set_avions_au_sol(std::vector<Avion*> val);
+
+    void ajouterSucc(const InfosAeroport*a);
+    void ajouterpoids(int a, int num_aero);
+
+    //void remove_avion_aero(Avion* avion);
+};
+
+
+#endif // HEADERAERO_H_INCLUDED
---
+++ Version_2.0/Projet Avion/Interface.cpp
@@ -0,0 +1,366 @@
+#include "Interface.h"
+
+//                                 #include <stdio.h>
+//                                 #include <stdlib.h>
+#define WIDTH 1556
+#define HEIGHT 815
+
+///CONSTRUCTEURS/DESTRUCTEURS
+Interface::Interface() {}
+Interface::~Interface() {}
+///METHODES
+void Interface::affichage_interface(DifferentsAeroports aeroports,int type)
+{
+    ///CREATION VARIABLES BUFFER+IMAGES+POLICES
+    BITMAP *page;
+    BITMAP *image;
+    BITMAP *grille;
+    FONT *cn;
+    ///INITIALISATION ALLEGRO
+    allegro_init();
+    install_keyboard();
+    install_mouse();
+    /// OUVERTURE MODE GRAPHIQUE
+    set_color_depth(desktop_color_depth());
+    ///ERREUR OUVERTURE MODE GRAPHIQUE
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,WIDTH,HEIGHT,0,0)!=0)
+    {
+        allegro_message("probleme mode graphique : %s", allegro_error);
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    ///DEFINITIONS COULEURS
+    int red = makecol(255,0,0);
+    int white = makecol(255,255,255);
+    //int blue = makecol(0,255,0);
+    //int green = makecol(0,0,255);
+    ///CREATION BUFFER
+    page=create_bitmap(SCREEN_W,SCREEN_H);
+    ///CHARGEMENT CARTE
+    image=load_bitmap("images/carte.bmp",NULL);
+    ///CHARGEMENT AVION
+    ///DEFINITION POILICE
+    cn=load_font("fonts/couriernew.pcx",NULL,NULL);
+    grille=load_bitmap("images/grille.bmp",NULL);
+    show_mouse(screen);
+    while(!key[KEY_ESC])
+    {
+        ///NETTOYAGE BUFFER
+        clear_bitmap(page);
+        ///ERREUR POLICE
+        if (!cn)
+        {
+            allegro_message("pas pu trouver/charger couriernew.pcx");
+            allegro_exit();
+            exit(EXIT_FAILURE);
+        }
+        ///ERREUR OUVERTURE IMAGE
+        if (!image)
+        {
+            allegro_message("pas pu trouver/charger image");
+            allegro_exit();
+            exit(EXIT_FAILURE);
+        }
+        ///AFFICHAGE BITMAP PAR DESSUS BUFFER
+        blit(image,page,0,0,0,0,SCREEN_W,SCREEN_H);
+        draw_sprite(page,grille,0,0);
+        ///DESSINS CERCLES SUR BUFFER
+        circlefill(page, 716, 204, 6, red); //paris
+        if((mouse_x>=710 && mouse_x<=722) && (mouse_y>=198 && mouse_y<=210))
+        {
+            textprintf_ex(page, cn,716, 204,red,white, "PARIS(716,204)"); ///////////////////////////////////////////utiliser findaeroport2 pour automatisation
+            textprintf_ex(page, cn,716, 220,red,white, "PISTES:3");
+            textprintf_ex(page, cn,716, 236,red,white, "PLACES_SOL:10");
+        }
+        circlefill(page, 391, 220, 6, red); //newyork
+        if((mouse_x>=385 && mouse_x<=397) && (mouse_y>=214 && mouse_y<=226))
+        {
+            textprintf_ex(page, cn,391, 220,red,white, "NEWYORK(391,220)");
+            textprintf_ex(page, cn,391, 236,red,white, "PISTES:5");
+            textprintf_ex(page, cn,391, 252,red,white, "PLACES_SOL:12");
+        }
+        circlefill(page, 347, 367, 6, red); //haiti
+        if((mouse_x>=341 && mouse_x<=353) && (mouse_y>=361 && mouse_y<=373))
+        {
+            textprintf_ex(page, cn,347, 367,red,white, "HAITI(347,367)");
+            textprintf_ex(page, cn,347, 383,red,white, "PISTES:1");
+            textprintf_ex(page, cn,347, 399,red,white, "PLACES_SOL:4");
+        }
+        circlefill(page, 371, 565, 6, red); //lapaz
+        if((mouse_x>=365 && mouse_x<=377) && (mouse_y>=559 && mouse_y<=571))
+        {
+            textprintf_ex(page, cn,371, 565,red,white, "LAPAZ(371,565)");
+            textprintf_ex(page, cn,371, 581,red,white, "PISTES:1");
+            textprintf_ex(page, cn,371, 597,red,white, "PLACES_SOL:6");
+        }
+        circlefill(page, 800, 657, 6, red); //capetown
+        if((mouse_x>=794 && mouse_x<=806) && (mouse_y>=651 && mouse_y<=663))
+        {
+            textprintf_ex(page, cn,800, 657,red,white, "CAPETOWN(800,657)");
+            textprintf_ex(page, cn,800, 673,red,white, "PISTES:2");
+            textprintf_ex(page, cn,800, 689,red,white, "PLACES_SOL:6");
+        }
+        circlefill(page, 1218, 402, 6, red); //bangkok
+        if((mouse_x>=1212 && mouse_x<=1224) && (mouse_y>=396 && mouse_y<=408))
+        {
+            textprintf_ex(page, cn,1218, 402,red,white, "BANGKOK(1218,402)");
+            textprintf_ex(page, cn,1218, 418,red,white, "PISTES:2");
+            textprintf_ex(page, cn,1218, 434,red,white, "PLACES_SOL:5");
+        }
+        circlefill(page, 1264, 653, 6, red); //perth
+        if((mouse_x>=1258 && mouse_x<=1270) && (mouse_y>=647 && mouse_y<=659))
+        {
+            textprintf_ex(page, cn,1264, 653,red,white, "PERTH(1264,653)");
+            textprintf_ex(page, cn,1264, 669,red,white, "PISTES:5");
+            textprintf_ex(page, cn,1264, 685,red,white, "PLACES_SOL:12");
+            textprintf_ex(page, cn,1264, 701,red,white, "DELAI_SOL:6UT");
+            textprintf_ex(page, cn,1264, 717,red,white, "ACCES_PISTES:2UT");
+        }
+        ///ARCS
+        //line(page,716, int y1, int y2, int color);
+        ///AFFICHAGE BUFFER
+        blit(page,screen,0,0,0,0,SCREEN_W,SCREEN_H);
+        //std::cout<<mouse_x<<","<<mouse_y<<std::endl;
+    }
+    allegro_exit(); // pr terminer le programme
+}
+
+void Interface::interface_simulation(AllAvions avions,DifferentsAeroports aeroports,Simulation simulation)
+{
+    ///CREATION VARIABLES BUFFER+IMAGES+POLICES
+    BITMAP *page;
+    BITMAP *image;
+    BITMAP *grille;
+    FONT *cn;
+    ///INITIALISATION ALLEGRO
+    allegro_init();
+    install_keyboard();
+    install_mouse();
+    /// OUVERTURE MODE GRAPHIQUE
+    set_color_depth(desktop_color_depth());
+    ///ERREUR OUVERTURE MODE GRAPHIQUE
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,WIDTH,HEIGHT,0,0)!=0)
+    {
+        allegro_message("probleme mode graphique : %s", allegro_error);
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    ///DEFINITIONS COULEURS
+    int red = makecol(255,0,0);
+    int white = makecol(255,255,255);
+    //int blue = makecol(0,255,0);
+    //int green = makecol(0,0,255);
+    ///CREATION BUFFER
+    page=create_bitmap(SCREEN_W,SCREEN_H);
+    ///CHARGEMENT CARTE
+    image=load_bitmap("images/carte.bmp",NULL);
+    ///CHARGEMENT AVIONS IMAGES
+    for(auto a:avions.get_liste_avions())
+    {
+        if(a->get_type()==1)
+        {
+            a->set_img(load_bitmap("images/avion_1.bmp",NULL));
+        }
+        else if(a->get_type()==2)
+        {
+            a->set_img(load_bitmap("images/avion_2.bmp",NULL));
+        }
+        else if(a->get_type()==3)
+        {
+            a->set_img(load_bitmap("images/avion_3.bmp",NULL));
+        }
+        ///ERREUR OUVERTURE IMAGE
+        if (!image || !a->get_img())
+        {
+            allegro_message("pas pu trouver/charger image");
+            allegro_exit();
+            exit(EXIT_FAILURE);
+        }
+    }
+    ///DEFINITION POLICE
+    cn=load_font("fonts/couriernew.pcx",NULL,NULL);
+    grille=load_bitmap("images/grille.bmp",NULL);
+    show_mouse(screen);
+    while(!key[KEY_ESC])
+    {
+        ///NETTOYAGE BUFFER
+        clear_bitmap(page);
+        ///ERREUR POLICE
+        if (!cn)
+        {
+            allegro_message("pas pu trouver/charger couriernew.pcx");
+            allegro_exit();
+            exit(EXIT_FAILURE);
+        }
+        ///AFFICHAGE BITMAP PAR DESSUS BUFFER
+        blit(image,page,0,0,0,0,SCREEN_W,SCREEN_H);
+        draw_sprite(page,grille,0,0);
+        ///DESSINS CERCLES SUR BUFFER
+        circlefill(page, 716, 204, 6, red); //paris
+        if((mouse_x>=710 && mouse_x<=722) && (mouse_y>=198 && mouse_y<=210))
+        {
+            /*Coordonnees c;
+            c.set_x(716);
+            c.set_y(204);
+            InfosAeroport v=findAeroport2(c,aeroports)
+            textprintf_ex(page, cn,716, 204,red,white, "%s",v->get_Nom()+"(716,204)");*/
+            textprintf_ex(page, cn,716, 204,red,white, "PARIS(716,204)"); ///////////////////////////////////////////utiliser findaeroport2 pour automatisation
+            textprintf_ex(page, cn,716, 220,red,white, "PISTES:3");
+            textprintf_ex(page, cn,716, 236,red,white, "PLACES_SOL:10");
+        }
+        circlefill(page, 391, 220, 6, red); //newyork
+        if((mouse_x>=385 && mouse_x<=397) && (mouse_y>=214 && mouse_y<=226))
+        {
+            textprintf_ex(page, cn,391, 220,red,white, "NEWYORK(391,220)");
+            textprintf_ex(page, cn,391, 236,red,white, "PISTES:5");
+            textprintf_ex(page, cn,391, 252,red,white, "PLACES_SOL:12");
+        }
+        circlefill(page, 347, 367, 6, red); //haiti
+        if((mouse_x>=341 && mouse_x<=353) && (mouse_y>=361 && mouse_y<=373))
+        {
+            textprintf_ex(page, cn,347, 367,red,white, "HAITI(347,367)");
+            textprintf_ex(page, cn,347, 383,red,white, "PISTES:1");
+            textprintf_ex(page, cn,347, 399,red,white, "PLACES_SOL:4");
+        }
+        circlefill(page, 371, 565, 6, red); //lapaz
+        if((mouse_x>=365 && mouse_x<=377) && (mouse_y>=559 && mouse_y<=571))
+        {
+            textprintf_ex(page, cn,371, 565,red,white, "LAPAZ(371,565)");
+            textprintf_ex(page, cn,371, 581,red,white, "PISTES:1");
+            textprintf_ex(page, cn,371, 597,red,white, "PLACES_SOL:6");
+        }
+        circlefill(page, 800, 657, 6, red); //capetown
+        if((mouse_x>=794 && mouse_x<=806) && (mouse_y>=651 && mouse_y<=663))
+        {
+            textprintf_ex(page, cn,800, 657,red,white, "CAPETOWN(800,657)");
+            textprintf_ex(page, cn,800, 673,red,white, "PISTES:2");
+            textprintf_ex(page, cn,800, 689,red,white, "PLACES_SOL:6");
+        }
+        circlefill(page, 1218, 402, 6, red); //bangkok
+        if((mouse_x>=1212 && mouse_x<=1224) && (mouse_y>=396 && mouse_y<=408))
+        {
+            textprintf_ex(page, cn,1218, 402,red,white, "BANGKOK(1218,402)");
+            textprintf_ex(page, cn,1218, 418,red,white, "PISTES:2");
+            textprintf_ex(page, cn,1218, 434,red,white, "PLACES_SOL:5");
+        }
+        circlefill(page, 1264, 653, 6, red); //perth
+        if((mouse_x>=1258 && mouse_x<=1270) && (mouse_y>=647 && mouse_y<=659))
+        {
+            textprintf_ex(page, cn,1264, 653,red,white, "PERTH(1264,653)");
+            textprintf_ex(page, cn,1264, 669,red,white, "PISTES:5");
+            textprintf_ex(page, cn,1264, 685,red,white, "PLACES_SOL:12");
+            textprintf_ex(page, cn,1264, 701,red,white, "DELAI_SOL:6UT");
+            textprintf_ex(page, cn,1264, 717,red,white, "ACCES_PISTES:2UT");
+        }
+        ///ARCS
+        //line(page,716, int y1, int y2, int color);
+        ///AVIONS
+        for(auto a:avions.get_liste_avions())
+        {
+            draw_sprite(page,a->get_img(),a->get_coo().get_x(),a->get_coo().get_y());
+            if((mouse_x>=a->get_coo().get_x() && mouse_x<=a->get_coo().get_x()+25) && (mouse_y>=a->get_coo().get_y() && mouse_y<=a->get_coo().get_y()+25))
+            {
+                //std::cout<<a->get_matricule()<<std::endl;
+                textprintf_ex(page, cn,a->get_coo().get_x()+25, a->get_coo().get_y()+25,red,white,"%d",a->get_matricule());
+                textprintf_ex(page, cn,a->get_coo().get_x()+25, a->get_coo().get_y()+25,red,white,"%d",a->get_matricule());
+            }
+        }
+        ///AFFICHAGE BUFFER
+        blit(page,screen,0,0,0,0,SCREEN_W,SCREEN_H);
+    }
+    allegro_exit(); // pr terminer le programme
+}
+
+void Interface::tracer_chemin(Voyageur& voyageur)
+{
+    int x1=0;
+    int y1=0;
+    int x2=0;
+    int y2=0;
+    std::stack<InfosAeroport*> chemin_a_tracer=(voyageur.get_avion()).get_trajet();
+
+    ///CREATION VARIABLES BUFFER+IMAGES+POLICES
+    BITMAP *page;
+    BITMAP *image;
+    BITMAP *grille;
+    FONT *cn;
+    ///INITIALISATION ALLEGRO
+    allegro_init();
+    install_keyboard();
+    install_mouse();
+    /// OUVERTURE MODE GRAPHIQUE
+    set_color_depth(desktop_color_depth());
+    ///ERREUR OUVERTURE MODE GRAPHIQUE
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,WIDTH,HEIGHT,0,0)!=0)
+    {
+        allegro_message("probleme mode graphique : %s", allegro_error);
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    ///DEFINITIONS COULEURS
+    int red = makecol(255,0,0);
+    //int blue = makecol(0,255,0);
+    //int green = makecol(0,0,255);
+    ///CREATION BUFFER
+    page=create_bitmap(SCREEN_W,SCREEN_H);
+    ///CHARGEMENT CARTE
+    image=load_bitmap("images/carte.bmp",NULL);
+
+    ///DEFINITION POLICE
+    cn=load_font("fonts/couriernew.pcx",NULL,NULL);
+    grille=load_bitmap("images/grille.bmp",NULL);
+    show_mouse(screen);
+
+    while(!key[KEY_ESC])
+    {
+        ///NETTOYAGE BUFFER
+        clear_bitmap(page);
+        ///ERREUR POLICE
+        if (!cn)
+        {
+            allegro_message("pas pu trouver/charger couriernew.pcx");
+            allegro_exit();
+            exit(EXIT_FAILURE);
+        }
+        ///AFFICHAGE BITMAP PAR DESSUS BUFFER
+        blit(image,page,0,0,0,0,SCREEN_W,SCREEN_H);
+        draw_sprite(page,grille,0,0);
+        ///DESSINS CERCLES SUR BUFFER
+        circlefill(page, 716, 204, 6, red); //paris
+        circlefill(page, 391, 220, 6, red); //newyork
+        circlefill(page, 347, 367, 6, red); //haiti
+        circlefill(page, 371, 565, 6, red); //lapaz
+        circlefill(page, 800, 657, 6, red); //capetown
+        circlefill(page, 1218, 402, 6, red); //bangkok
+        circlefill(page, 1264, 653, 6, red); //perth
+
+        if(chemin_a_tracer.size()!=0)
+        {
+            unsigned int taille=chemin_a_tracer.size();
+            for(unsigned int i=0;i<taille;i++)
+            {
+                //Enregistrer les prochains points
+                x2=((chemin_a_tracer.top())->get_Localisation()).get_x();
+                y2=((chemin_a_tracer.top())->get_Localisation()).get_y();
+
+                //Si pas le premier aeroport
+                if(x1!=0 && y1!=0)
+                {
+                    //tracer un trait de point1 à point2
+                    line(page, x1,y1,x2,y2,red);
+                }
+                //On enregistre x1 et y1 pour le prochain aeroport
+                x1=x2;
+                y1=y2;
+                //On retire l'aeroport actuel
+                chemin_a_tracer.pop();
+                ///AFFICHAGE BUFFER
+                blit(page,screen,0,0,0,0,SCREEN_W,SCREEN_H);
+            }
+        }
+        //Tant que la pile n'est pas vide
+
+    }
+    allegro_exit(); // pr terminer le programme
+}
+
